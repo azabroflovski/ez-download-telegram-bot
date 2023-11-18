eb4872441d4af0e0365ef0fe33cd5f14ba2677d6
azabroflovski <azabroflovski@gmail.com>
2023-11-19 03:56:57 +0500

    feat: bot can download media from tiktok

--- handlers/message.text.ts
+++ handlers/message.text.ts
@@ -3,13 +3,16 @@ import { detectUrlPlatform, isUrl } from '../helpers/url'
 import { sleep } from '../helpers/promise'
 // @ts-ignore
 import getInstagramMediaUrl from '@sasmeee/igdl'
+// @ts-ignore
+import getTiktokMediaUrl from '@sasmeee/tkdl'
 
 const bot = new Composer()
 
 bot.on('message:text', async (ctx, next) => {
-    await ctx.replyWithChatAction('typing')
 
     if (!isUrl(ctx.message.text)) {
+        await ctx.replyWithChatAction('typing')
+        await sleep(1000)
         await ctx.reply(`Отправьте ссылку, бот автоматически определит ресурс и загрузит медиа файл, без необходимости подписок и дополнительных шагов.`)
         return
     }
@@ -17,6 +20,7 @@ bot.on('message:text', async (ctx, next) => {
     const platform = detectUrlPlatform(ctx.message.text)
 
     if (platform.name === 'unknown') {
+        await ctx.replyWithChatAction('typing')
         await sleep(2000)
         await ctx.reply(`
 К сожалению, бот не смог определить медиа файл по указанному URL адресу. 
@@ -29,10 +33,9 @@ bot.on('message:text', async (ctx, next) => {
         return
     }
 
-    const loadingMessage = await ctx.reply('⏳', {
-        reply_to_message_id: ctx.message.message_id,
-    })
 
+
+    await ctx.replyWithChatAction('typing')
     const replyOptions = {
         reply_to_message_id: ctx.message.message_id,
         caption: 'Скачано с помощью @ez_download_bot',
@@ -41,20 +44,53 @@ bot.on('message:text', async (ctx, next) => {
         }
     }
 
-    await ctx.replyWithChatAction('typing')
+    await sleep(500)
+    const loadingMessage = await ctx.reply('⏳', {
+        reply_to_message_id: ctx.message.message_id,
+    })
 
     if (platform.name === 'instagram') {
+        interface InstagramMediaResponse {
+            thumbnail_link: string
+            download_link: string
+        }
 
-        const { download_link } = (await getInstagramMediaUrl(platform.url.href))[0] as { download_link: string }
+        const { download_link } = (await getInstagramMediaUrl(platform.url.href))[0] as InstagramMediaResponse
 
         if (download_link.includes('jpg') || download_link.includes('jpeg')) {
+            await ctx.replyWithChatAction('upload_photo')
+            await sleep(500)
             await ctx.replyWithPhoto(download_link, replyOptions)
         } else {
+            await ctx.replyWithChatAction('upload_video')
+            await sleep(500)
             await ctx.replyWithVideo(download_link, replyOptions)
         }
 
-        await ctx.api.deleteMessage(ctx.chat.id, loadingMessage.message_id)
+        return
     }
+
+
+    if (platform.name === 'tiktok') {
+
+        interface TiktokMediaResponse {
+            title: string
+            author: string
+            sd: string
+            hd: string
+            audio: string
+            thumbnail: string
+        }
+
+        await ctx.replyWithChatAction('upload_video')
+        await sleep(1000)
+
+        const { sd } = (await getTiktokMediaUrl(platform.url.href))[0] as TiktokMediaResponse
+
+        await ctx.replyWithVideo(sd, replyOptions)
+    }
+
+    await ctx.api.deleteMessage(ctx.chat.id, loadingMessage.message_id)
 })
 
 export default bot
